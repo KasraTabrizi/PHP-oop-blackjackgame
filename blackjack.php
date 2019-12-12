21f26c86e8dc4a9d0b763076c9a7edd1a1fe208f
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-12 13:26:22 +0100

    tranfered html code from game.html to home.html

--- blackjack.php
+++ blackjack.php
@@ -1,7 +1,23 @@
 <?php
 
     class Blackjack{
-        
+
+        //properties
+        $score = 0;
+
+        //methods
+        function Hit(){
+            return rand(1 , 11); //return a number between 1 and 11
+        }
+
+        function Stand(){
+            
+        }
+
+        function Surrender(){
+            return true;
+        }
+
     }
 
 ?>
