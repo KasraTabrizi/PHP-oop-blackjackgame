46b53e531211d2aff87c481b1e96391833fe3be6
KasraTabrizi <kasra_tabrizi@hotmail.com>
2019-12-12 16:06:35 +0100

    added scss and css  files and included in home.php

--- home.php
+++ home.php
@@ -5,7 +5,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Black Jack PHP</title>
+    <style>
+    <?php include 'styles/css/main_styles.css' ?>
+    </style>
 </head>
 
 <body>
